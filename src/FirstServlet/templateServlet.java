3a5e36b5fbff5cb6ff7a90fc3a805d1faa9f8839
boteman0206 <1197426145@qq.com>
2019-11-25 23:43:55 +0800

    模板方式实现servlet的注意点

--- src/FirstServlet/templateServlet.java
+++ src/FirstServlet/templateServlet.java
@@ -6,15 +6,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class 模板方式实现servlet extends HttpServlet {
+public class templateServlet extends HttpServlet {
 
-    /**
+    // TODO: 2019/11/25 模板方式实现servlet
+    /**      继承关系
+     *  HttpServlet ——》 GenericServlet --> Servlet
      *  get 请求
      */
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+//        super.doGet(req, resp);  todo 这一行代码需要注释掉 不然会报错
+
+        resp.getWriter().write("模板方式实现servlet");
 
     }
 
@@ -24,6 +28,6 @@ public class 模板方式实现servlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+//        super.doPost(req, resp);
     }
 }
